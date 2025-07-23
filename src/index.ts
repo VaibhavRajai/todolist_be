c2ab6c41bbc85c070da6164b95a8845ef046fea0
VaibhavRajai <rajaivaibhav28@gmail.com>
2025-07-23 17:32:29 +0530

    jwt

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import connectDB from './config/db';
 import authRoutes from './routes/auth';
+import cors from 'cors'
 const app = express();
+app.use(cors())
 const PORT = 3000;
 
 app.use(express.json()); 
