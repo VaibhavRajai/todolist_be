cb324fa0e9441c56142809a16e3547f87fcd9dfc
VaibhavRajai <rajaivaibhav28@gmail.com>
2025-07-23 17:16:28 +0530

    jwt

--- dist/api/index.js
+++ dist/api/index.js
@@ -4,8 +4,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const db_1 = __importDefault(require("./config/db"));
-const auth_1 = __importDefault(require("./routes/auth"));
+const db_1 = __importDefault(require("../config/db"));
+const auth_1 = __importDefault(require("../routes/auth"));
 const app = (0, express_1.default)();
 const PORT = 3000;
 app.use(express_1.default.json());
---
+++ src/api/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import connectDB from '../config/db';
+import authRoutes from '../routes/auth';
+const app = express();
+const PORT = 3000;
+
+app.use(express.json()); 
+connectDB();
+
+app.use('/api', authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
